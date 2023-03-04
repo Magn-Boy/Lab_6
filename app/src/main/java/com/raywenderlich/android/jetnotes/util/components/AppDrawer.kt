22e267e13d6b0c8405a3aa022818b287f83d0695
Secure <andruha.vasilev.2004@mail.ru>
2023-03-04 16:12:02 +0300

    Changed file AppDrawer.kt, strings.xml

--- app/src/main/java/com/raywenderlich/android/jetnotes/util/components/AppDrawer.kt
+++ app/src/main/java/com/raywenderlich/android/jetnotes/util/components/AppDrawer.kt
@@ -1,13 +1,14 @@
+@file:Suppress("UNUSED_EXPRESSION")
+
 package com.raywenderlich.android.jetnotes.util.components
 
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
@@ -19,7 +20,10 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.raywenderlich.android.jetnotes.R
+import com.raywenderlich.android.jetnotes.routing.JetNotesRouter
+import com.raywenderlich.android.jetnotes.routing.Screen
 import com.raywenderlich.android.jetnotes.theme.JetNotesTheme
+import com.raywenderlich.android.jetnotes.theme.JetNotesThemeSettings
 
 @Composable
 private fun AppDrawerHeader(){
@@ -102,6 +106,63 @@ private fun ScreenNavigationButton(
     }
 }
 
+@Composable
+private fun LightDarkThemeItem(){
+Row(
+    Modifier
+        .padding(8.dp)
+) {
+    Text(
+        text = stringResource(id = R.string.on_dark_theme),
+        style = MaterialTheme.typography.body2,
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.6f),
+        modifier = Modifier
+            .weight(1f)
+            .padding(start = 8.dp, end = 8.dp, top = 8.dp, bottom = 8.dp)
+            .align(alignment = Alignment.CenterVertically)
+    )
+    Switch(
+        checked = JetNotesThemeSettings.isDarkThemeEnabled,
+        onCheckedChange ={ JetNotesThemeSettings.isDarkThemeEnabled = it},
+        modifier = Modifier
+            .padding(start = 8.dp, end = 8.dp)
+            .align(alignment = Alignment.CenterVertically)
+    )
+}
+}
+
+@Composable
+fun AppDrawer(
+    currentScreen: Screen,
+    closeDrawerAction: () ->Unit
+) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        AppDrawerHeader()
+
+        Divider(color = MaterialTheme.colors.onSurface.copy(alpha = .2f))
+
+        ScreenNavigationButton(
+            icon = Icons.Filled.Home,
+            label = stringResource(id = R.string.notes),
+            isSelected = currentScreen == Screen.Notes,
+            onClick = {
+                JetNotesRouter.navigateTo(Screen.Notes)
+                closeDrawerAction()
+            }
+        )
+        ScreenNavigationButton(
+            icon = Icons.Filled.Delete,
+            label = stringResource(id = R.string.basket),
+            isSelected = currentScreen == Screen.Trash,
+            onClick = {
+                JetNotesRouter.navigateTo(Screen.Trash)
+                closeDrawerAction
+            }
+        )
+        LightDarkThemeItem()
+    }
+}
+
 @Preview
 @Composable
 fun ScreenNavigationButtonPreview(){
@@ -112,5 +173,20 @@ fun ScreenNavigationButtonPreview(){
             isSelected = true,
             onClick = {}
         )
+    }
+}
+@Preview
+@Composable
+private fun LightDarkThemeItemPreview(){
+    JetNotesTheme {
+        LightDarkThemeItem()
+    }
+}
+
+@Preview
+@Composable
+fun AppDrawerPreview(){
+    JetNotesTheme {
+        AppDrawer(Screen.Notes) {}
     }
 }
