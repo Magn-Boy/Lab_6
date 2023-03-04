a35d66f82c0653a9f8e53761f24ee5ffc04b7bb5
Secure <andruha.vasilev.2004@mail.ru>
2023-03-05 02:21:49 +0300

    Changed file MainActivity.kt, MainViewModel.kt. Add file NotesScreens.kt, TopAppBar.kt

--- app/src/main/java/com/raywenderlich/android/jetnotes/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/jetnotes/MainActivity.kt
@@ -4,17 +4,10 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.Scaffold
-import androidx.compose.material.ScaffoldState
-import androidx.compose.material.rememberScaffoldState
-import androidx.compose.runtime.rememberCoroutineScope
-import com.raywenderlich.android.jetnotes.routing.Screen
 import com.raywenderlich.android.jetnotes.theme.JetNotesTheme
-import com.raywenderlich.android.jetnotes.util.components.AppDrawer
-import com.raywenderlich.android.jetnotes.util.components.Note
+import com.raywenderlich.android.jetnotes.util.screens.NotesScreen
 import com.raywenderlich.android.jetnotes.viewmodel.MainViewModel
 import com.raywenderlich.android.jetnotes.viewmodel.MainViewModelFactory
-import kotlinx.coroutines.launch
 
 /**
  * Main activity приложения.
@@ -34,25 +27,7 @@ class MainActivity : AppCompatActivity() {
 
     setContent {
       JetNotesTheme {
-        val coroutineScope = rememberCoroutineScope()
-        val scaffoldState: ScaffoldState = rememberScaffoldState()
-
-        Scaffold(
-          scaffoldState = scaffoldState,
-          drawerContent = {
-            AppDrawer(
-              currentScreen = Screen.Notes,
-              closeDrawerAction = {
-                coroutineScope.launch {
-                  scaffoldState.drawerState.close()
-                }
-              }
-            )
-          },
-          content = {
-            Note()
-          }
-        )
+        NotesScreen(viewModel = viewModel)
       }
     }
   }
---
+++ app/src/main/java/com/raywenderlich/android/jetnotes/util/screens/NotesScreens.kt
@@ -0,0 +1,13 @@
+package com.raywenderlich.android.jetnotes.util.screens
+
+import androidx.compose.runtime.Composable
+import com.raywenderlich.android.jetnotes.viewmodel.MainViewModel
+import com.raywenderlich.android.jetnotes.util.screens.NotesScreen
+
+
+@Composable
+fun NotesScreen(
+    viewModel: MainViewModel
+){
+
+}
